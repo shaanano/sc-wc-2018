9b5634f27828c91e84d21bcab41adbad0de0e12f
Omri Shaanan <omris@LP-TLV-OMRIS.local>
2018-07-02 16:17:14 +0300

    added json endpoint for KO

--- server.js
+++ server.js
@@ -138,8 +138,7 @@ var server = http.createServer(function (req, res) {
             doCalcKO()
             if (req.url === "/json") {
                 res.writeHead(200, { 'Content-Type': 'application/json' })
-                var resObject = { "pointsOnly": players, "withPosition": playersWithPosition }
-                res.end(JSON.stringify(resObject))
+                res.end(JSON.stringify(players_ko))
             }
             else {
                 res.writeHead(200, { 'Content-Type': 'text/html' })
@@ -167,6 +166,8 @@ var server = http.createServer(function (req, res) {
                                 case 2:
                                 index = 11
                                 break
+                                case 4:
+                                index = 
                                 default:
                                 index = j/2 + 7
                             }
